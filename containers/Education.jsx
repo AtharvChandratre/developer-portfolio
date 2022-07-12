3b04daf6800aca01671cd9b58d374d786478458b
Atharv Chandratre <atharvchandratre@gmail.com>
2022-07-12 14:56:08 +0530

    Update Education.jsx

--- containers/Education.jsx
+++ containers/Education.jsx
@@ -22,7 +22,7 @@ const Edution = () => {
 						return (
 							<Col
 								className="order-lg-1"
-								lg="6"
+								lg="12"
 								key={info.schoolName}
 							>
 								<EdutionCard education={info} />
