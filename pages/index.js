cbf01fe26f9d1f8e3dde7530f63a16fb0990ae0b
Atharv Chandratre <atharvchandratre@gmail.com>
2022-12-01 17:15:37 -0600

    Put website under development

--- pages/index.js
+++ pages/index.js
@@ -17,65 +17,71 @@ import SEO from "../components/SEO";
 
 export default function Home({ githubProfileData }) {
 	return (
-		<div>
-			<SEO
-				data={{
-					title: "Atharv Chandratre | Portfolio",
-					description:
-						"I'm a passionate technologist interested in web and blockchain technologies. I love to explore the disruptive potential of blockchain in present-day systems, especially at its intersection with finance. In my free time, I play badminton, do origami and overthink.",
-					image: "https://media-exp1.licdn.com/dms/image/C5603AQGK1Zc_CNCXaA/profile-displayphoto-shrink_400_400/0/1639934873557?e=1648684800&v=beta&t=yu3ybHoVwOaa5rfX4yro_3pM0tuYv2pmM512A6fc37A",
-						url: "https://developer-portfolio-atharvchandratre.vercel.app/",
-					keywords: [
-						"Atharv",
-						"Atharv Chandratre",
-						"Portfolio",
-						"Atharv Portfolio ",
-						"Atharv Chandratre Portfolio",
-						"blockchain",
-						"solidity",
-						"chainlink",
-						"cryptocurrency",
-						"crypto",
-						"smart contract",
-						"smart contracts",
-						"infura",
-						"truffle",
-						"truffle suite",
-						"decentralized",
-						"decenttralized application",
-						"decentralized applications",
-						"dapps",
-						"dapp",
-						"blockchain developer",
-						"full stack blockchain developer",
-						"web developer",
-						"full stack",
-						"full stack web developer",
-						"mobile app developer",
-						"android developer",
-						"django",
-						"flask",
-						"django rest framework",
-						"nodejs ",
-						"expressjs",
-						"reactjs ",
-						"contextapi",
-						"redux",
-						"flutter"
-					],
-				}}
-			/>
-			<Navigation />
-			<Greetings />
-			<Skills />
-			{/* <Proficiency /> */}
-			<Experience />
-			<Education />
-			<Projects />
-			<TeachingExperience />
-			<LeadershipExperience />
-			<Podcasts />
-			<ContactMe />
+		<div className="text-center">
+			<h1>
+		This website is being rebuilt. Thank you for your patience!
+			</h1>
 		</div>
+		
+		// <div>
+		// 	<SEO
+		// 		data={{
+		// 			title: "Atharv Chandratre | Portfolio",
+		// 			description:
+		// 				"I'm a passionate technologist interested in web and blockchain technologies. I love to explore the disruptive potential of blockchain in present-day systems, especially at its intersection with finance. In my free time, I play badminton, do origami and overthink.",
+		// 			image: "https://media-exp1.licdn.com/dms/image/C5603AQGK1Zc_CNCXaA/profile-displayphoto-shrink_400_400/0/1639934873557?e=1648684800&v=beta&t=yu3ybHoVwOaa5rfX4yro_3pM0tuYv2pmM512A6fc37A",
+		// 				url: "https://developer-portfolio-atharvchandratre.vercel.app/",
+		// 			keywords: [
+		// 				"Atharv",
+		// 				"Atharv Chandratre",
+		// 				"Portfolio",
+		// 				"Atharv Portfolio ",
+		// 				"Atharv Chandratre Portfolio",
+		// 				"blockchain",
+		// 				"solidity",
+		// 				"chainlink",
+		// 				"cryptocurrency",
+		// 				"crypto",
+		// 				"smart contract",
+		// 				"smart contracts",
+		// 				"infura",
+		// 				"truffle",
+		// 				"truffle suite",
+		// 				"decentralized",
+		// 				"decenttralized application",
+		// 				"decentralized applications",
+		// 				"dapps",
+		// 				"dapp",
+		// 				"blockchain developer",
+		// 				"full stack blockchain developer",
+		// 				"web developer",
+		// 				"full stack",
+		// 				"full stack web developer",
+		// 				"mobile app developer",
+		// 				"android developer",
+		// 				"django",
+		// 				"flask",
+		// 				"django rest framework",
+		// 				"nodejs ",
+		// 				"expressjs",
+		// 				"reactjs ",
+		// 				"contextapi",
+		// 				"redux",
+		// 				"flutter"
+		// 			],
+		// 		}}
+		// 	/>
+		// 	<Navigation />
+		// 	<Greetings />
+		// 	<Skills />
+		// 	{/* <Proficiency /> */}
+		// 	<Experience />
+		// 	<Education />
+		// 	<Projects />
+		// 	<TeachingExperience />
+		// 	<LeadershipExperience />
+		// 	<Podcasts />
+		// 	<ContactMe />
+		// </div>
 	);
 }
